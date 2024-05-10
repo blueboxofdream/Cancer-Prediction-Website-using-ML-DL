e5b066dd373d29fa093d6936f939e839fec6618c
Saimanti Mullick <112518522+blueboxofdream@users.noreply.github.com>
2024-05-11 03:50:39 +0530

    app.py

--- app.py
+++ app.py
@@ -220,8 +220,8 @@ async def read_geo(request: Request):
     return templates.TemplateResponse("geo.html", {"request": request})
 
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+# if __name__ == '__main__':
+#     uvicorn.run(app, host='0.0.0.0', port=8000)
 
 
 
